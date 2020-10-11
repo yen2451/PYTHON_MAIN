ec11600bf075ce278f20d660e8a67c38f0a74db4
chiayen <38468171+yen2451@users.noreply.github.com>
2020-10-11 13:54:04 +0800

    test??

--- cacl.py
+++ cacl.py
@@ -1,15 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 def f(x):
     return x**3-5*x**2+200
-
 
 sequence = [-4.572, -4.5718, -4.5716, -4.5714, -4.5712, -4.571]
 for i in sequence:
     print(i, " ", f(i))
-
 
 plt.xlabel("x")
 plt.ylabel("y")
