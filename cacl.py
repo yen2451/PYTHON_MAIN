d1f8b3100f398b0777f2ccafa641335f57e56393
chiayen <38468171+yen2451@users.noreply.github.com>
2020-10-11 13:51:47 +0800

    edit cacl.py

--- cacl.py
+++ cacl.py
@@ -1,11 +1,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def f(x):
     return x**3-5*x**2+200
 
-sequence = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-x = np.linspace(-6,-2, num=100)
+
+sequence = [-4.572, -4.5718, -4.5716, -4.5714, -4.5712, -4.571]
+for i in sequence:
+    print(i, " ", f(i))
+
+
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("Figure of $x^{3}$-5*$x^{2}$+200")
+x = np.linspace(-4.572, -4.57, num=100)
 y = f(x)
+
 plt.plot(x, y)
+plt.savefig('fig1.jpg')
 plt.show()
