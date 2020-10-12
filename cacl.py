7a55de5fb33f4aeed2928f935fd0bad9b6941bae
chiayen <38468171+yen2451@users.noreply.github.com>
2020-10-12 13:51:10 +0800

    add axis title, graph title

--- cacl.py
+++ cacl.py
@@ -1,19 +1,27 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 def f(x):
+    # function
     return x**3-5*x**2+200
+
 
 sequence = [-4.572, -4.5718, -4.5716, -4.5714, -4.5712, -4.571]
 for i in sequence:
     print(i, " ", f(i))
-
+# 顯示Ｘ座標標題
 plt.xlabel("x")
-plt.ylabel("y")
+# 顯示Ｙ座標標題
+plt.ylabel("f(x)")
+# 顯示圖表標題
 plt.title("Figure of $x^{3}$-5*$x^{2}$+200")
-x = np.linspace(-4.572, -4.57, num=100)
+# plot fuction's graph
+x = np.linspace(-4.571, -4.572, num=100)
 y = f(x)
-
-plt.plot(x, y)
-plt.savefig('fig1.jpg')
+plt.plot(x, y, linewidth=1, label="f(x)")
+# plot y=0 graph
+plt.plot([-4.571, -4.572], [0, 0], c='b', linestyle='dashed', label="y=0")
+# graph index
+plt.legend(loc='upper left')
 plt.show()
